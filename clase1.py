5c0817766882e313f661965041cb0bfb2a6e32d1
Martin <martin.fede.morales@gmail.com>
2023-07-04 20:45:21 -0300

    Ejercicios hechos en clase

---
+++ clase1.py
@@ -0,0 +1,14 @@
+# str   
+# numericas: int float
+# bool booleano ----> V o F
+
+def btn_mostrar_on_click(self):
+    #str
+    #numericas: int float
+    #bool booleano V o F. True o False
+    #entrada de un dto
+    
+    division = prompt("Pregunta", "Que divios es?")
+
+    alert("Hola", division)
+    #print(mensaje)  # literal de str
